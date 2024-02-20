e123b201df1c5896f7fa8a688a970b92829795da
felixrp74 <felixrp74@gmail.com>
2024-02-20 11:01:54 -0500

    institucionorigen data table server vuetify

--- src/views/admin/intitucionorigen/index.vue
+++ src/views/admin/intitucionorigen/index.vue
@@ -1,49 +1,23 @@
 <template>
   <v-toolbar>
-    <v-list-item
-      title="Institucion de origen"
-      subtitle=""
-    />
+    <v-list-item title="Institucion de origen" subtitle="" />
     <v-spacer></v-spacer>
     <v-btn prepend-icon="mdi-plus" variant="tonal" @click="newRecord">
       Nuevo
     </v-btn>
   </v-toolbar>
-  <v-data-table-server
-    class="border"
-    v-model:items-per-page="itemsPerPage"
-    :headers="headers"
-    :items-length="totalItems"
-    :items="serverItems"
-    :loading="loading"
-    :search="search"
-    item-value="Name"
-    :items-per-page-options="[1, 5, 10, 25, 50]"
-    @update:options="loadItems"
-  >
-    <template v-slot:[`item.Status`]="{ item }">
-      <v-chip :color="item.Status ? 'blue' : 'error'" dark label small>
-        {{ item.Status ? "Activo" : "Inactivo" }}
-      </v-chip>
-    </template>
+  <v-data-table-server class="border" v-model:items-per-page="itemsPerPage" :headers="headers" :items-length="totalItems"
+    :items="serverItems" :loading="loading" :search="search" item-value="Name"
+    :items-per-page-options="[1, 5, 10, 25, 50]" @update:options="loadItems">
+
     <template v-slot:item.actions="{ item }">
-      <v-btn
-        icon
-        @click="editItem(item)"
-        class="mr-2"
-        color="green darken-4"
-        density="compact"
-        variant="tonal"
-      >
+      <v-btn icon @click="editItem(item)" class="mr-2" color="green darken-4" density="compact" variant="tonal">
         <v-icon>mdi-pencil</v-icon>
       </v-btn>
 
       <v-btn icon color="red" density="compact" variant="tonal">
-        <DialogConfirm
-          @onConfirm="deleteItem(item)"
-          :title="`Eliminar ${item.Name}`"
-          :text="`¿Está seguro de que desea eliminar el tipo de documento ${item.Name}?`"
-        ></DialogConfirm>
+        <DialogConfirm @onConfirm="deleteItem(item)" :title="`Eliminar ${item.nombre}`"
+          :text="`¿Está seguro de que desea eliminar el tipo de documento ${item.nombre}?`"></DialogConfirm>
         <v-icon>mdi-delete</v-icon>
       </v-btn>
     </template>
@@ -79,7 +53,7 @@
 
 <script setup lang="ts">
 import axios from "axios";
-//import DialogConfirm from "@/components/DialogConfirm.vue";
+import DialogConfirm from "@/components/DialogConfirm.vue";
 import { ref } from "vue";
 
 const listDocumentTypes = ref([]);
@@ -94,18 +68,20 @@ const search = ref("");
 const dialog = ref(false);
 const editedItem = ref({});
 const defaultItem = ref({
-  nombre:"",
+  id: "",
+  nombre: "",
   codigo: "",
   ubigeo: "",
-  pais_id: "",
-  tipo: ""
+  tipo: "",
+  pais_id: ""
 });
 const editedIndex = ref(-1);
 
 const loadItems = async ({ page, itemsPerPage, sortBy, search }) => {
   loading.value = true;
 
-  let res = await axios.get("http://segundas.unap.pe/api/institucionesOrigen", {
+  // let res = await axios.get("http://segundas.unap.pe/api/institucionesOrigen", {
+  let res = await axios.post("http://servicio_institucionorigen.test/api/institucionorigens", {
     page,
     itemsPerPage,
     sortBy,
@@ -117,7 +93,7 @@ const loadItems = async ({ page, itemsPerPage, sortBy, search }) => {
 
   headers.value = data.headers;
   totalItems.value = data.length;
-  serverItems.value = data.data;
+  serverItems.value = data.items.data;
 
   console.log("page", page);
   console.log("itemsPerPage", itemsPerPage);
@@ -145,21 +121,23 @@ const close = () => {
 
 const saveRecord = async () => {
   if (editedIndex.value === -1) {
-    let res = await axios.post("http://segundas.unap.pe/api/institucionOrigen", {
+    // let res = await axios.post("http://segundas.unap.pe/api/institucionOrigen", {
+    let res = await axios.post("http://servicio_institucionorigen.test/api/institucionorigensstore", {
       nombre: editedItem.value.nombre,
       codigo: editedItem.value.codigo,
       ubigeo: editedItem.value.ubigeo,
       pais_id: editedItem.value.pais_id,
       tipo: editedItem.value.tipo
     });
-    serverItems.value.push({ Status: 1, ...res.data });
+    serverItems.value.push({ ...res.data.data });
 
     dialog.value = false;
   } else {
     console.log("editedItem", editedItem.value);
 
     let res = await axios.patch(
-      "http://segundas.unap.pe/api/institucionOrigen/" + editedItem.value.Id,
+      // "http://segundas.unap.pe/api/institucionOrigen/" + editedItem.value.id,
+      "http://servicio_institucionorigen.test/api/institucionorigens/" + editedItem.value.id,
       {
         nombre: editedItem.value.nombre,
         codigo: editedItem.value.codigo,
@@ -169,15 +147,16 @@ const saveRecord = async () => {
       }
     );
 
-    serverItems.value[editedIndex.value] = res.data;
-
+    serverItems.value[editedIndex.value] = res.data.data;
+    dialog.value = false;
     console.log("update record");
   }
 };
 
 const deleteItem = async (item) => {
   let res = await axios.delete(
-    "http://segundas.unap.pe/api/institucionOrigen/" + item.Id
+    // "http://segundas.unap.pe/api/institucionOrigen/" + item.id
+    "http://servicio_institucionorigen.test/api/institucionorigens/" + item.id
   );
 
   serverItems.value.splice(serverItems.value.indexOf(item), 1);
