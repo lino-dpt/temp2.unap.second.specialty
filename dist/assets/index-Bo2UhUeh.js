c325a8843ca7de061a2d79d70d6541c28e6050b6
lino <denis.puma@unap.edu.pe>
2024-02-22 08:37:33 -0500

    Remove "dist" from .gitignore

---
+++ dist/assets/index-Bo2UhUeh.js
@@ -0,0 +1,1 @@
+import{a9 as r}from"./index-BjvFM0dB.js";const e={};function c(n,s){return"usuarios"}const o=r(e,[["render",c]]);export{o as default};
---
+++ dist/assets/index-CLqpfOWo.js
@@ -0,0 +1,1 @@
+import{a9 as e}from"./index-BjvFM0dB.js";const r={};function c(n,t){return" Seguridad "}const a=e(r,[["render",c]]);export{a as default};
