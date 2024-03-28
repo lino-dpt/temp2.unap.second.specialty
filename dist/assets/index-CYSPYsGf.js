182c1e6657bfe3b4c586543f53a255573eb4b157
lino <nearlino20@gmail.com>
2024-03-28 08:28:38 -0500

    Delete unused files and update script source in index.html

---
+++ dist/assets/index-CYSPYsGf.js
@@ -0,0 +1,1 @@
+import{af as e,o as c,l as n}from"./index-Cq5_0v14.js";const o={};function r(t,a){return c(),n("h1",null,"comfiguraciones")}const f=e(o,[["render",r]]);export{f as default};
---
+++ dist/assets/index-DYQoiMiH.js
@@ -0,0 +1,1 @@
+import{af as r}from"./index-Cq5_0v14.js";const e={};function c(n,s){return"usuarios"}const o=r(e,[["render",c]]);export{o as default};
